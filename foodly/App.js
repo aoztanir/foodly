4e3191251c603b0afb295a209b308c46d829225e
Aryah Oztanir <aoztanir25@gmail.com>
2021-03-04 08:33:25 -0800

    second commit

--- foodly/App.js
+++ foodly/App.js
@@ -7,7 +7,7 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import { createStackNavigator } from '@react-navigation/stack';
 
 
-
+import
 
 import AsyncStorage from '@react-native-community/async-storage';
 import HomeScreen from './screens/HomeScreen';
